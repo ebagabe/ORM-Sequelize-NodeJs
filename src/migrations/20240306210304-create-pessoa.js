22f9b86485cc2a6285cf844a32e7cd21e19912ce
ebagabe <gabriel.ebagabe@gmail.com>
2024-03-06 18:19:52 -0300

    Fazendo a primeira migração com SQLite

--- src/migrations/20240306210304-create-pessoa.js
+++ src/migrations/20240306210304-create-pessoa.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Pessoas', {
+    await queryInterface.createTable('pessoas', {
       id: {
         allowNull: false,
         autoIncrement: true,
