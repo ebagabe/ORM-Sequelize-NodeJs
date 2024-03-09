f36746ae39eb4df8977f1b7aa47c1589e3373f1b
ebagabe <gabriel.ebagabe@gmail.com>
2024-03-09 10:53:03 -0300

    Criando rota e Controller

---
+++ src/controllers/PessoaController.js
@@ -0,0 +1,11 @@
+class PessoaController {
+    static async pegaTodos(req, res) {
+        try {
+            // Acessar o modelo
+        } catch (error) {
+            // Erro
+        }
+    }
+}
+
+module.exports = PessoaController;
